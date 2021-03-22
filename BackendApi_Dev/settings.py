1b856294f759b8d0c1660be019ce4409a1e26d54
Neal <neal@localhost.localdomain>
2021-03-22 12:26:39 +0300

    upadates on requirementes.py file

--- BackendApi_Dev/settings.py
+++ BackendApi_Dev/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'okta_oauth2.middleware.OktaMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'BackendApi_Dev.urls'
